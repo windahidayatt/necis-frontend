10ce3027f84fd23a0f87c4577a8d9342aa1552b8
windahidayatt <winda.amelia.tif18@polban.ac.id>
2020-07-11 07:54:08 +0700

    (+) Modify BASE URL

--- src/config/BaseURL.js
+++ src/config/BaseURL.js
@@ -1,2 +1,4 @@
 // export const BASE_URL = 'https://jsonplaceholder.typicode.com'
-export const BASE_URL = 'http://127.0.0.1:8000'
+// export const BASE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://necis.online'
+
